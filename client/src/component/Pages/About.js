b3f25e87066f8d8535afc29114751ef1db274802
unknown <ugwuisaaciu@gmail.com>
2021-01-22 18:16:32 +0100

    new commit2

--- client/src/component/Pages/About.js
+++ client/src/component/Pages/About.js
@@ -19,7 +19,8 @@ export default class About extends Component {
     <div class="col-lg-7 col-md-6">
       <div class="about-content wow fadeInLeft">
         <h2>About UicSoft</h2>
-        <h3>Odio sed id eos et laboriosam consequatur eos earum soluta.</h3>
+        <h3></h3>
+          <p>UicSoft offers a professional tech Service to businesses. We will listen and talk through all of your ideas, starting with the 'dream idea' and working out what is affordable and achievable to suit your budget. If you are a new business just starting out, then we can help you achieve your new identity, whatever your budget. Let us help you SHINE in the busy marketplace. </p>
             <ul>
           <li><i class="ion-android-checkmark-circle"></i>  Smart, Light and Adaptive.</li>
           <li><i class="ion-android-checkmark-circle"></i> Stand out –We have uniqee Design.</li>
@@ -30,7 +31,17 @@ export default class About extends Component {
                                         <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60"
                                             aria-valuemin="0" aria-valuemax="100" style={{width: '100%'}}></div>
                                     </div>
-                                    <span>WordPress/Woo-commerce <small>80%</small></span>
+                                    <span>Graphic Design <small>95%</small></span>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80"
+                                            aria-valuemin="0" aria-valuemax="100" style={{width: '95%'}}></div>
+                                    </div>
+                                    <span>Video Editing <small>90%</small></span>
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80"
+                                            aria-valuemin="0" aria-valuemax="100" style={{width: '90%'}}></div>
+                                    </div>
+                                    <span>WordPress/Woo-commerce and Jumla <small>80%</small></span>
                                     <div class="progress">
                                         <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80"
                                             aria-valuemin="0" aria-valuemax="100" style={{width: '80%'}}></div>
@@ -40,10 +51,10 @@ export default class About extends Component {
                                         <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="90"
                                             aria-valuemin="0" aria-valuemax="100" style={{width: '90%'}}></div>
                                     </div>
-                                    <span>Custom Branding <small>70%</small></span>
+                                    <span>Custom Branding <small>80%</small></span>
                                     <div class="progress">
-                                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="70"
-                                            aria-valuemin="0" aria-valuemax="100" style={{width: '70%'}}></div>
+                                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80"
+                                            aria-valuemin="0" aria-valuemax="100" style={{width: '80%'}}></div>
                                     </div>
       </div>
     </div>
